ee217b0aa022676214164472b8c29d3dcd459ae9
Yousef Shanawany <askyous@gmail.com>
2016-08-20 18:37:28 -0700

    Circles and texts displaying

--- script.ts
+++ script.ts
@@ -1,7 +1,25 @@
-let data = ['red', 'blue'];
+let svg = document.getElementsByTagName('svg')[0];
 
-d3.selectAll('.item')
-    .data(data)
-    .style('background', d => {
-      return d;
-    });
+$.getJSON('sample-data/people.json', people => {
+
+  people.forEach(person => {
+
+    // Circle
+    let circle = <SVGCircleElement> document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    circle.setAttribute('cx', (Math.random() * svg.scrollWidth).toString());
+    circle.setAttribute('cy', (Math.random() * svg.scrollHeight).toString());
+    circle.setAttribute('r', "10");
+    svg.appendChild(circle);
+
+    // Text
+    let text = <SVGTextElement> document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    text.innerHTML = person;
+    text.setAttribute('text-anchor', 'middle');
+    text.setAttribute('x', circle.cx.animVal.value.toString());
+    text.setAttribute('y', (circle.cy.animVal.value + circle.r.animVal.value + 20).toString());
+    svg.appendChild(text);
+
+  });
+
+
+});
