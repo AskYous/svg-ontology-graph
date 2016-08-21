9eda75964b41338fb539f2ecc84cebdca50a5b8a
Yousef Shanawany <askyous@gmail.com>
2016-08-21 09:43:48 -0700

    Converted to TypeScript classes

--- script.ts
+++ script.ts
@@ -1,66 +1,36 @@
-let svg = document.getElementsByTagName('svg')[0];
-let ns = 'http://www.w3.org/2000/svg';
+/// <reference path="./declarations/person.d.ts"/>
 
-let radius = 20;
+let svg: SVGGElement = document.getElementsByTagName('svg')[0] as any;
 
-let svgPadding = 30;
-svg.style.padding = `${svgPadding}px`;
-let svgWidth = svg.scrollWidth - (svgPadding * 2);
-let svgHeight = svg.scrollHeight - (svgPadding * 2);
+let people: Array<Person>;
+let relations: Array<Array<number>>;
 
-$.getJSON('sample-data/people.json', people => {
+// Get People
+$.getJSON('sample-data/people.json', peopleResults => {
+    $.getJSON('sample-data/relations.json', relationsResults => {
+        people = peopleResults;
+        relations = relationsResults;
+        begin(); // Go!
+    });
+});
+
+function begin() {
+  let vertices = new Array<Vertex>();
 
   // Display in SVG
   people.forEach((person, i) => {
-
-    // Group
-    let g = <SVGGElement> document.createElementNS(ns, 'g');
-    g.id = person.id;
-
-    // Circle
-    let circle = <SVGCircleElement> document.createElementNS(ns, 'circle');
-    circle.setAttribute('cx', (Math.random() * svgWidth).toString());
-    circle.setAttribute('cy', (Math.random() * (svgHeight- (svgPadding - radius))).toString());
-    circle.setAttribute('r', radius.toString());
-    g.appendChild(circle);
-
-    // Text
-    let text = <SVGTextElement> document.createElementNS(ns, 'text');
-    text.innerHTML = person.name;
-    text.setAttribute('text-anchor', 'middle');
-    text.setAttribute('x', circle.cx.animVal.value.toString());
-    text.setAttribute('y', (circle.cy.animVal.value + circle.r.animVal.value + 20).toString());
-    g.appendChild(text);
-
-    svg.appendChild(g);
-
+      let vertex = new Vertex(person.id, person.name, svg);
+      vertices.push(vertex);
+      vertex.display(svg);
   });
 
-  $.getJSON('sample-data/relations.json', relations => {
-    relations.forEach(relation => {
-        let studentId = <number> relation[0];
-        let teacherId = <number> relation[1];
+  relations.forEach(relation => {
+      let studentId = <number>relation[0];
+      let teacherId = <number>relation[1];
 
-        let studentGroup: SVGGElement =  document.getElementById(studentId.toString()) as any;
-        let teacherGroup: SVGGElement =  document.getElementById(teacherId.toString()) as any;
+      let studentVertex = vertices.filter(v => v.id == studentId)[0];
+      let teacherVertex = vertices.filter(v => v.id == teacherId)[0];
 
-        let studentCircle: SVGCircleElement = studentGroup.getElementsByTagNameNS(ns, 'circle')[0] as any;
-        let teacherCircle: SVGCircleElement = teacherGroup.getElementsByTagNameNS(ns, 'circle')[0] as any;
-
-        let line = <SVGLineElement> document.createElementNS(ns, 'line');
-
-        // Starting position
-        line.setAttribute('x1', studentCircle.cx.animVal.value.toString());
-        line.setAttribute('y1', studentCircle.cy.animVal.value.toString());
-
-        // Ending position
-        line.setAttribute('x2', teacherCircle.cx.animVal.value.toString());
-        line.setAttribute('y2', teacherCircle.cy.animVal.value.toString());
-
-        // Other properties
-
-        svg.insertBefore(line, svg.childNodes[0]);
-    });
+      studentVertex.lineTo(teacherVertex);
   });
-
-});
+}
